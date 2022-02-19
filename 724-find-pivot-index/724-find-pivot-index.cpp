7579a62cf2651dfa8d5e776fe76315269c4d9057
Ritika lohia <56797604+ritikalohia@users.noreply.github.com>
2022-02-19 11:13:04 +0530

    Time: 60 ms (8.55%), Space: 32.7 MB (6.01%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int> vect;
+        vect.push_back(nums[0]);
+        for(int i=1; i<nums.size(); i++)
+            vect.push_back(nums[i] + vect[i-1]);
+        
+        int n = vect.size();
+        int last = vect[n-1];
+        int first=0;
+        for(int i=0; i<n; i++){
+            if(i!=0)
+                first= vect[i-1];
+            int otherside = last-vect[i];
+
+            if(first==otherside)
+                return i;
+        }
+        return -1;
+    }
+};
